0a8426f6cf480f8912b57e33ccd100d1554d2f68
Skubivi <altair100001@gmaIl.com>
2021-05-23 04:10:00 +0300

    Добавлено изменение размера зарядов

--- Elementary Charges/Elementary Charges/Main.cpp
+++ Elementary Charges/Elementary Charges/Main.cpp
@@ -4,7 +4,11 @@
 
 using namespace sf;
 
-void draw(std::vector<Charge>&charges, RenderWindow &window) {
+void draw(std::vector<Charge>&charges, RenderWindow &window, CircleShape prototype, float size) {
+	window.clear();
+	prototype.setRadius(size);
+	window.draw(prototype);
+
 	for (int i = 0; i < charges.size(); i++){
 		charges[i].draw(window);
 	}
@@ -16,6 +20,10 @@ int main()
 	RenderWindow window(VideoMode(1600, 900), "Elementary Charges");
 	std::vector<Charge>charges;
 	float chargeSize = 10.0f;
+
+	CircleShape prototype = CircleShape(chargeSize);
+	prototype.setFillColor(Color(150, 150, 150));
+	prototype.setPosition(1000.0f, 0.0f);
 	
 
 	while (window.isOpen())
@@ -25,15 +33,18 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
-			if (event.type == sf::Event::MouseButtonPressed) {
+			if (event.type == Event::MouseButtonPressed) {
 				if (event.mouseButton.button == sf::Mouse::Left)
 					charges.push_back(Charge(event.mouseButton.x - chargeSize, event.mouseButton.y - chargeSize, false, chargeSize));
 				else
 					charges.push_back(Charge(event.mouseButton.x - chargeSize, event.mouseButton.y - chargeSize, true, chargeSize));
 			}
+			if (event.type == Event::MouseWheelScrolled) {
+				chargeSize += 5 * event.mouseWheelScroll.delta;
+			}
 		}
 
-		draw(charges, window);
+		draw(charges, window, prototype, chargeSize);
 	}
 
 	return 0;
